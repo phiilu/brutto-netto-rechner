c386bda33f199e54d7bfa79d1f9451288f125b27
Florian Kapfenberger <florian@kapfenberger.me>
2020-11-21 23:40:41 +0100

    feat: add plausible analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,11 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <script
+        async
+        defer
+        data-domain="bruttonetto.phiilu.com"
+        src="https://p.phiilu.com/js/plausible.js"></script>
     </Layout>
   );
 }
